672befa31cc309f99f8fe7504a0fc9f64a8c1cbd
ChrisMaher3 <christophermaher2003@gmail.com>
2024-09-24 17:00:20 +0100

    Differential Equation Method

---
+++ R2.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate
+
+v = 10 
+r = 50 
+l = 100
+
+def differential_rl(t, i):
+    dIdt = (v-r*i)/l
+    return dIdt
+
+def main():
+    i0 = np.array([0])
+    t0 = 0
+    tf = 20
+    h = 0.5
+    t = np.arange(t0, tf+h, h)
+    result = integrate.solve_ivp(fun=differential_rl, 
+                                 t_span=(t0, tf), 
+                                 y0=i0, 
+                                 method="RK45", 
+                                 t_eval=t)
+    i_differential = result.y[0]
+    t = result.t
+    plt.plot(t, i_differential, "k.", label = "Differential Equation")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Current (A)")
+    plt.legend()
+    plt.savefig("R2_v10_r50_l100__h0.5_dif.svg",bbox_inches='tight') 
+    plt.show()
+    
+if __name__ == '__main__':
+    main()
