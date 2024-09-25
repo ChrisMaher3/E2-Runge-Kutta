29ca87418385ef28625292de925ebaeff1d1b32a
ChrisMaher3 <christophermaher2003@gmail.com>
2024-09-25 13:06:04 +0100

    Updates

--- R2.py
+++ R2.py
@@ -14,7 +14,7 @@ def main():
     i0 = np.array([0])
     t0 = 0
     tf = 20
-    h = 0.005
+    h = 0.5
     t = np.arange(t0, tf+h, h)
     result = integrate.solve_ivp(fun=differential_rl, 
                                  t_span=(t0, tf), 
@@ -25,12 +25,12 @@ def main():
     t = result.t
     i_exact = (v / r) * (1 - np.exp(-r * t / l))
     
-    plt.plot(t, i_differential, "k.", label = "Differential Equation")
-    plt.plot(t, i_exact, "C1.", label = "Exact Solution")
+    plt.plot(t, i_differential, "k-", label = "Differential Equation")
+    plt.plot(t, i_exact, "C1--", label = "Exact Solution")
     plt.xlabel("Time (s)")
     plt.ylabel("Current (A)")
     plt.legend()
-    #plt.savefig("R2_Exact_vs_Dif_h0.005.svg",bbox_inches='tight') 
+    plt.savefig("R2_Exact_vs_Dif_h0.5.svg",bbox_inches='tight') 
     plt.show()
     
 if __name__ == '__main__':
